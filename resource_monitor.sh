c5b3383bbd00c2e192d53d7aad87cd0557c15ab6
gayeonni <gayeoneez@gmail.com>
2024-07-24 14:29:17 +0900

    Update resource_monitor.sh

--- resource_monitor.sh
+++ resource_monitor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 LOG_FILE=/var/log/resource_usage.log
 timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 cpu_usage=$(top -b -n1 | grep 'Cpu(s)' | awk '{print $2 + $4}')
 mem_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
-disk_usage=$(df -h | grep /dev/sda1 | awk '{print $5}')
-
+disk_usage=$(df -h | grep '/dev/sda1' | awk '{print $5}')
 echo "$timestamp, CPU: $cpu_usage%, Memory: $mem_usage%, Disk: $disk_usage" >> $LOG_FILE
+chown ubuntu:ubuntu $LOG_FILE
+chmod 644 $LOG_FILE
