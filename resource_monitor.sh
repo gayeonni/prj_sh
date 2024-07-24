749d1a31e04f2a4825dfcce49d6a1cd0d779cf7a
gayeonni <gayeoneez@gmail.com>
2024-07-24 15:28:19 +0900

    Update resource_monitor.sh

--- resource_monitor.sh
+++ resource_monitor.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
+# 리소스 사용량을 기록할 로그 파일
 LOG_FILE="/var/log/resource_usage.log"
-REMOTE_LOG_SERVER="10.0.1.59"
-REMOTE_LOG_PATH="/home/ubuntu/logs-1"
 
 # 현재 날짜 및 시간
 timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
-# 애플리케이션 프로세스 ID 찾기
-app_pid=$(pgrep -f app)
-app2_pid=$(pgrep -f app2)
+# 애플리케이션 프로세스 ID 찾기 (JAR 파일 기준)
+app_pid=$(ps -ef | grep '[j]ava -jar /home/ubuntu/inclass-spring-security-0.0.1-SNAPSHOT.jar' | awk '{print $2}')
 
 if [ -n "$app_pid" ]; then
     # app의 CPU 및 메모리 사용량
@@ -20,17 +18,8 @@ else
     mem_usage="N/A"
 fi
 
-if [ -n "$app2_pid" ]; then
-    # app2의 CPU 및 메모리 사용량
-    cpu_usage2=$(ps -p $app2_pid -o %cpu=)
-    mem_usage2=$(ps -p $app2_pid -o %mem=)
-else
-    cpu_usage2="N/A"
-    mem_usage2="N/A"
-fi
-
 # 로그 파일에 기록
-echo "$timestamp, app CPU: $cpu_usage%, app Memory: $mem_usage%, app2 CPU: $cpu_usage2%, app2 Memory: $mem_usage2%" >> $LOG_FILE
+echo "$timestamp, app CPU: $cpu_usage%, app Memory: $mem_usage%" >> $LOG_FILE
 
 # 로그 파일을 원격 서버로 전송
-scp $LOG_FILE user@$REMOTE_LOG_SERVER:$REMOTE_LOG_PATH
+scp $LOG_FILE user@a-log:/home/ubuntu/logs-1
